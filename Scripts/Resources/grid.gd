2748a7fb22398e0c4241b9d74376d88be2c4e9b0
OrangeJuiceD108 <zachtundra57@gmail.com>
2025-10-17 23:22:21 -0500

    More additions to grid
    get_cell, is_within_bounds, and as_index functions completed

--- Scripts/Resources/grid.gd
+++ Scripts/Resources/grid.gd
@@ -1,10 +1,25 @@
 class_name Grid extends Resource
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var size := Vector2(20,20)
+@export var cell_size := Vector2(16, 16)
+
+# TODO: STORE MAP
+
+func get_cell(loc: Vector2) -> Vector2:
+	return (loc/cell_size).floor()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# Should check when a given position (not tile) is within grid bounds
+func is_within_bounds(loc: Vector2) -> bool:
+	return 0 < loc.x and loc.x < cell_size.x * size.x and 0 < loc.y and loc.y < cell_size.y * size.y
+
+
+# TODO: clamp()
+# Should return a given grid position as clamped within grid bounds
+func clamp(grid_loc: Vector2) -> Vector2:
+	return grid_loc
+
+
+# Should return a given grid positon as an index to a 1D array
+func as_index(grid_loc: Vector2) -> int:
+	return int(grid_loc.x + grid_loc.y * size.x)
