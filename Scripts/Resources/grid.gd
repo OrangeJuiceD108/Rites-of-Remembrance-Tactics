139941d123b944f09aa22a1193c92f833731504d
OrangeJuiceD108 <zachtundra57@gmail.com>
2025-10-17 23:29:27 -0500

    More grid changes
    Finished clamp function, grid is done until I decide how to store maps

--- Scripts/Resources/grid.gd
+++ Scripts/Resources/grid.gd
@@ -14,10 +14,12 @@ func is_within_bounds(loc: Vector2) -> bool:
 	return 0 < loc.x and loc.x < cell_size.x * size.x and 0 < loc.y and loc.y < cell_size.y * size.y
 
 
-# TODO: clamp()
 # Should return a given grid position as clamped within grid bounds
 func clamp(grid_loc: Vector2) -> Vector2:
-	return grid_loc
+	var out := grid_loc
+	out.x = clamp(out.x, 0, size.x - 1.0)
+	out.y = clamp(out.y, 0, size.y - 1.0)
+	return out
 
 
 # Should return a given grid positon as an index to a 1D array
