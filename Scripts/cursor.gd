f3ee8b7fc4314a25d5d7b3e00827f5350238bfb0
OrangeJuiceD108 <zachtundra57@gmail.com>
2025-10-17 23:38:18 -0500

    Cursor work
    Cursor now tracks mouse position

--- Scripts/cursor.gd
+++ Scripts/cursor.gd
@@ -1,3 +1,11 @@
 extends Node
 
+var cursor_position := Vector2.ZERO
+
 # TODO: Function to get mouse input
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		cursor_position = event.position
+	elif event is InputEventMouseButton:
+		# TODO: Stuff happens here
+		pass
