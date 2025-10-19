47450f4d8785c2cf78f99b3268b8aec428a9fb35
OrangeJuiceD108 <zachtundra57@gmail.com>
2025-10-19 01:34:14 -0500

    Battle scene
    Added a battle scene that has a cursor and a map manager
    Created scrips for map manager and unit manager

---
+++ Scripts/Managers/map_manager.gd
@@ -0,0 +1,3 @@
+class_name Map_Manager extends Node
+
+@export var grid : Grid
--- Scripts/cursor.gd
+++ Scripts/cursor.gd
@@ -3,10 +3,11 @@ class_name Cursor extends Node
 
 var cursor_position := Vector2.ZERO
 
+@onready var map_manager := get_node("../Map_Manager")
 
+# TODO: Needs to check how recent the last input was so that we don't do the same input 20 times
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		cursor_position = event.position
 	elif event is InputEventMouseButton:
-		# TODO: Stuff happens here
-		pass
+		print(map_manager.grid.is_within_bounds(event.position))
