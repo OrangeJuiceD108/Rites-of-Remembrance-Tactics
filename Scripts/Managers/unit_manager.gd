0a9b1c40134624d9198d555651609ca3a447ce4a
OrangeJuiceD108 <zachtundra57@gmail.com>
2026-05-25 19:40:52 -0500

    A little bit of cleaning up

--- Scripts/Managers/unit_manager.gd
+++ Scripts/Managers/unit_manager.gd
@@ -6,7 +6,8 @@ var occupied_tiles: Array[Vector2i]
 @onready var player_manager = get_node("Player Unit Manager") as Player_Unit_Manager
 @onready var ally_manager = get_node_or_null("Allied Unit Manager") as Allied_Unit_Manger
 
-enum State {IDLE, UNIT_SELECTED} # TODO: I'll need more states at some point I'm sure
+# TODO: I'll need more states at some point I'm sure
+enum State {IDLE, UNIT_SELECTED, UNIT_MOVING, ACTION_SELECTED} 
 var state = State.IDLE
 
 func _ready():
@@ -17,38 +18,45 @@ func _ready():
 func _on_cell_clicked(cell: Vector2i):
 	match state: 
 		State.IDLE:
-			var unit = player_manager.get_unit_at_cell(cell)
-			if unit:
-				state = State.UNIT_SELECTED
-				player_manager.select_unit(unit)
-			else:
-				# TODO: This should try to get a unit here for enemy, then ally. If neither work, should bring up the menu I think
-				pass
+			_handle_idle_click(cell)
 		State.UNIT_SELECTED:
-			# TODO: Cell w/ unit logic
-			# Basically builds the menu that should come up, along with temporarily moving the unit
-			# Cases that we need to handle here:
-			#  - Spot is empty and within move range (Wait/Items)
-			#    - and has enemy within attack range (Attack)
-			#    - and has speakable character within speaking range (Talk)
-			#    - and has player within interaction range (Trade/Rescue)
-			#    - and has player within spell range (Heal/Teleport)
-			#  - (NOTE: This can be implemented later) Spot is not empty, but spot meets one of the following conditions: 
-			#    - Spot has enemy and is within attack range of a reachable empty spot (Highlight Attack)
-			#    - Spot has speakable character and is within speaking range of another reachable spot (Highlight Speak)
-			#    - Spot has player and is within interaction range of another reachable spot (Highlight Trade)
-			#    - Spot has player and is within heal range of another reachable spot (Highlight Heal)
-			#  - Spot fails all above conditions (Ignore Event or Deselect)
-			# This would be a really great spot for one of those number enums, 
-			# so that each of these menu buttons can just be an integer that we add to a number
-			# Possible setup:
-			# - 128 = Talk
-			# - 64  = Attack
-			# - 32  = Heal
-			# - 16  = Teleport
-			# - 8   = Rescue
-			# - 4   = Trade
-			# - 2   = Items
-			# - 1   = Wait
-			pass
+			_handle_unit_selected_click(cell)
+	pass
+
+func _handle_idle_click(cell: Vector2i):
+	var unit = player_manager.get_unit_at_cell(cell)
+	if unit:
+		state = State.UNIT_SELECTED
+		player_manager.select_unit(unit)
+	else:
+		# TODO: This should try to get a unit here for enemy, then ally. If neither work, should bring up the menu I think
+		pass
+	pass
+
+func _handle_unit_selected_click(cell: Vector2i):
+	# TODO: Cell w/ unit logic
+	# Basically builds the menu that should come up, along with temporarily moving the unit
+	# Cases that we need to handle here:
+	#  - Spot is empty and within move range (Wait/Items); NOTE: State -> UNIT_MOVING
+	#    - and has enemy within attack range (Attack)
+	#    - and has speakable character within speaking range (Talk)
+	#    - and has player within interaction range (Trade/Rescue)
+	#    - and has player within spell range (Heal/Teleport)
+	#  - (NOTE: This can be implemented later) Spot is not empty, but spot meets one of the following conditions: 
+	#    - Spot has enemy and is within attack range of a reachable empty spot (Open Attack Menu; State -> ACTION_SELECTED, Auto Select Target)
+	#    - Spot has speakable character and is within speaking range of another reachable spot (Open Actions Menu, Highlight Talk; State -> UNIT_MOVING)
+	#    - Spot has player and is within interaction range of another reachable spot (Open Actions Menu, Highlight Trade; STATE -> UNIT_MOVING)
+	#    - Spot has player and is within heal range of another reachable spot (Open Heal Menu; State -> ACTION_SELECTED, Auto Select Target)
+	#  - Spot fails all above conditions (Ignore Event or Deselect)
+	# This would be a really great spot for one of those number enums, 
+	# so that each of these menu buttons can just be an integer that we add to a number
+	# Possible setup:
+	# - 128 = Talk
+	# - 64  = Attack
+	# - 32  = Heal
+	# - 16  = Teleport
+	# - 8   = Rescue
+	# - 4   = Trade
+	# - 2   = Items
+	# - 1   = Wait
 	pass
